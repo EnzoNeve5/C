c16b8e74ca5afcc43f17d971690a32b4e480004d
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2024-08-23 21:34:34 -0300

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+# include <math.h>
+
+int main() {
+    float r;
+    printf("O valor do raio do circulo e de: ");
+    scanf("%f",&r);
+    printf("O valor da area do circulo e de %.2f", M_PI * (r * r));
+    return 0;
+}
